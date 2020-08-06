c966ffda69a0c412c1a6cf3f02d5b22101e1730b
Tim Aadland <potetstapp@gmail.com>
2020-08-06 15:16:34 +0200

    So far it is only 25% chance to get it right, added a hint for the players. That show the three frist letters and the remaining with (-).

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+
+
 import random
 
 
@@ -6,9 +8,20 @@ print("Under construction.")
 
 guessing_word = random.choice(['python', 'java', 'kotlin', 'javascript'])
 
-user_guess = input("Guess the word: ").lower()
+show_three_letter = guessing_word[:3]
+# How long the word is, minus the first 3 letters
+length = len(guessing_word) - len(show_three_letter)
+# Multiple the length with "-" for hide the right word
+hidden = "-" * length
+# Add the three first visible letters and then the hidden ones.
+hint = show_three_letter + hidden
+
+# Let the user  see the hint before typing
+user_guess = input("Guess the word: " + hint + " ").lower()
 
 if guessing_word == user_guess:
     print("You survived!")
 else:
     print("You are hanged!")
+
+
