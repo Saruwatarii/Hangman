e267d2b7de1d01fb3aac479045d1cb607831ef5a
Tim Aadland <potetstapp@gmail.com>
2020-08-09 00:22:57 +0200

    Fixed error where the hidden word where not reset when the game was finished, from losing or winning.

--- hangman.py
+++ hangman.py
@@ -3,14 +3,14 @@ import random
 print("H A N G M A N")
 
 guessing_word = random.choice(['python', 'java', 'kotlin', 'javascript'])
-show_three_letter = guessing_word[:3]
+
 # How long the word is, minus the first 3 letters
-length = len(guessing_word) - len(show_three_letter)
+length = len(guessing_word)
 # Multiple the length with "-" for hide the right word
 # hidden = "-" * length
 hidden = "-" * len(guessing_word)
 # Add the three first visible letters and then the hidden ones.
-hint = show_three_letter + hidden
+
 # Let the user  see the hint before typing
 # user_guess = input("Guess the word: " + hint + " ").lower()
 # A list containing letters the user have guessed which not in the hidden word.
@@ -38,6 +38,9 @@ while flag:
 
             if guessing_word == hidden:
                 print(f"{guessing_word}\nYou guessed the word!\nYou survived!")
+                guessing_word = random.choice(['python', 'java', 'kotlin', 'javascript'])
+                hidden = "-" * len(guessing_word)
+                already_used.clear()
                 break
             # Check whether the letter is in the hidden word and is in the already_used list, if not in both, it gets added to the list
             if (user_guess not in guessing_word) and (user_guess not in already_used):
@@ -70,6 +73,12 @@ while flag:
                 continue
         else:
             print("You are hanged!")
+            tries = 8
+
+            guessing_word = random.choice(['python', 'java', 'kotlin', 'javascript'])
+            hidden = "-" * len(guessing_word)
+            already_used.clear()
+
     elif player == "exit":
         flag = False
     else:
